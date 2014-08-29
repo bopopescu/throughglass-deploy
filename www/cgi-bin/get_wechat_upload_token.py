b2bad0f79af72a4a88851ba14546171d3f9db3b8
guanghik <guanghik@gmail.com>
2014-08-30 06:11:15 +0800

    up

--- www/cgi-bin/get_wechat_upload_token.py
+++ www/cgi-bin/get_wechat_upload_token.py
@@ -1,46 +1,32 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-__author__ = 'yowenlove'
 
-# enable debugging
-import cgitb
-
-cgitb.enable()
-
-import sys
 from ye2pack import pack_utils, works_pb2
 from ye2pack.pack_pb2 import Packet
 from ye2pack.works_pb2 import GetWeChatUploadToken
 from model import usr_info
 from wxapi import update_token
 
-#
-# print "Content-Type: text/plain;charset=utf-8"
-# print
 
-#
-req_buf = sys.stdin.read()
-req_pkt = pack_utils.pre_decode(buf=req_buf)
-uin = req_pkt.uin
+def process(req_buf):
+    req_pkt = pack_utils.pre_decode(buf=req_buf)
+    uin = req_pkt.uin
 
-usr = usr_info.query(uin)
-session = usr.get('session')
+    usr = usr_info.query(uin)
+    session = usr.get('session')
 
-req_pkt = pack_utils.decode(buf=req_buf, key=session)
-req = GetWeChatUploadToken.Request()
-req.ParseFromString(req_pkt.data)
+    req_pkt = pack_utils.decode(buf=req_buf, key=session)
+    req = GetWeChatUploadToken.Request()
+    req.ParseFromString(req_pkt.data)
 
+    resp = GetWeChatUploadToken.Response()
+    resp.base_response.err_code = works_pb2.ERR_NONE
+    resp.token = update_token.get_token()
 
-resp = GetWeChatUploadToken.Response()
-resp.base_response.err_code = works_pb2.ERR_NONE
-resp.token = update_token.get_token()
-
-resp_buf = pack_utils.encode(
-    buf=resp.SerializeToString(),
-    encrypt=Packet.CRYPT_NONE,
-    key=session,
-    uin=uin
-)
-
-print resp_buf
+    resp_buf = pack_utils.encode(
+        buf=resp.SerializeToString(),
+        encrypt=Packet.CRYPT_NONE,
+        key=session,
+        uin=uin
+    )
+    return resp_buf
--- www/cgi-bin/throughglass.py
+++ www/cgi-bin/throughglass.py
@@ -3,11 +3,15 @@
 import web
 
 import auth
+import get_wechat_upload_token
+import post
 import update_wechat_account
 
 urls = (
     '/cgi-bin/auth.py', 'AuthHandler',
     '/cgi-bin/update_wechat_account.py', 'UpdateWeChatAccountHandler',
+    '/cgi-bin/post.py', 'PostHandler',
+    '/cgi-bin/get_wechat_upload_token.py', 'GetWeChatUploadTokenHandler',
 )
 
 app = web.application(urls, globals())
@@ -37,6 +41,32 @@ class UpdateWeChatAccountHandler:
     def GET(self):
         # web.header('Content-Type', 'text/plain')
         return update_wechat_account.process(web.data())
+
+
+class PostHandler:
+    def __init__(self):
+        return
+
+    def POST(self):
+        # web.header('Content-Type', 'text/plain')
+        return post.process(web.data())
+
+    def GET(self):
+        # web.header('Content-Type', 'text/plain')
+        return post.process(web.data())
+
+
+class GetWeChatUploadTokenHandler:
+    def __init__(self):
+        return
+
+    def POST(self):
+        # web.header('Content-Type', 'text/plain')
+        return get_wechat_upload_token.process(web.data())
+
+    def GET(self):
+        # web.header('Content-Type', 'text/plain')
+        return get_wechat_upload_token.process(web.data())
 
 
 application = app.wsgifunc()
