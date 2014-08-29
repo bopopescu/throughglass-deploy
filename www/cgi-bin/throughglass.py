ae152b39939470f5ebe68aaacd5b6c50a0a11df0
guanghik <guanghik@gmail.com>
2014-08-30 05:55:28 +0800

    up

--- www/cgi-bin/throughglass.py
+++ www/cgi-bin/throughglass.py
@@ -5,23 +5,37 @@ import web
 import auth
 import update_wechat_account
 
-urls = {
-    ('/cgi-bin/auth.py', 'AuthHandler'),
-    ('/cgi-bin/update_wechat_account.py', 'UpdateWeChatAccountHandler'),
-}
+urls = (
+    '/auth.py', 'AuthHandler',
+    '/update_wechat_account.py', 'UpdateWeChatAccountHandler',
+)
 
 app = web.application(urls, globals())
 
 
 class AuthHandler:
+    def __init__(self):
+        return
+
     def POST(self):
-        web.header('Content-Type', 'text/plain')
+        # web.header('Content-Type', 'text/plain')
+        return auth.process(web.data())
+
+    def GET(self):
+        # web.header('Content-Type', 'text/plain')
         return auth.process(web.data())
 
 
 class UpdateWeChatAccountHandler:
+    def __init__(self):
+        return
+
     def POST(self):
-        web.header('Content-Type', 'text/plain')
+        # web.header('Content-Type', 'text/plain')
+        return update_wechat_account.process(web.data())
+
+    def GET(self):
+        # web.header('Content-Type', 'text/plain')
         return update_wechat_account.process(web.data())
 
 
